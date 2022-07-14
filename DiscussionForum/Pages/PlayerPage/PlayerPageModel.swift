694f80352f9a69ef0338f40e87fbb75bac98689e
kautsiitd <kautsiitd@gmail.com>
2022-07-14 13:05:32 +0530

    Added PlayerPageVC class and it's Model

---
+++ DiscussionForum/Pages/PlayerPage/PlayerPageModel.swift
@@ -0,0 +1,22 @@
+//
+//  PlayerPageModel.swift
+//  DiscussionForum
+//
+//  Created by Kautsya Kanu on 14/07/22.
+//
+
+import Foundation
+
+class PlayerPageModel: BaseCoreLodableModel {
+    //MARK: - Properties
+    var movies: [Movie] = []
+    
+    //MARK: - CoreLoadable
+    override var fileName: String { "Movies" }
+    override var fileExtension: String { "json" }
+    
+    override func parse(_ data: Data) throws {
+        let decoder = JSONDecoder()
+        movies = try decoder.decode([Movie].self, from: data)
+    }
+}
--- DiscussionForum/Pages/PlayerPage/PlayerPageVC.swift
+++ DiscussionForum/Pages/PlayerPage/PlayerPageVC.swift
@@ -9,5 +9,13 @@ import UIKit
 
 class PlayerPageVC: UIViewController {
     //MARK: - Elements
+    @IBOutlet private var xrayMainView: UIView!
+    //MARK: - Properties
+    var model: PlayerPageModel? = nil
     
+    //MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        model = PlayerPageModel(viewController: self)
+    }
 }
